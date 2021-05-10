aa73eee134b62b636c0a83d128216ffbc266f12f
LewinTamey <71181409+LewinTamey@users.noreply.github.com>
2021-05-10 09:57:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from random import randint
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "!", description = "")
+
+@bot.event
+async def on_ready():
+    print("Ready !")
+
+@bot.command()
+async def nomg(ctx):
+    with open("garcon.txt", "r") as fichier:
+        lines = fichier.readlines()
+        element = lines[randint(0, len(lines)-1)]
+        await ctx.send(element)
+
+@bot.command()
+async def nomf(ctx):
+    with open("fille.txt", "r") as fichier:
+        lines = fichier.readlines()
+        element = lines[randint(0, len(lines)-1)]
+        await ctx.send(element)
+
+bot.run("ODMyNTY1NTYzMzM0NDU5NDIz.YHlpGg.ugMfjtvaYGQvMXZNhov2SOYM3Cw")
